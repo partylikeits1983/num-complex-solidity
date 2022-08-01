1ddfe713b189cd7cb9f82404a2be5cf8dbcf6b65
partylikeits1983 <aleqvids@gmail.com>
2022-08-01 21:24:39 +0300

    basic test

--- tests/test.test.js
+++ tests/test.test.js
@@ -1,23 +1,42 @@
 const { expect } = require("chai");
 const { parseUnits } = require("ethers/lib/utils");
 const { ethers, network } = require("hardhat");
+const Complex = require("complex.js");
 
 describe("Deploy Complex", () => {
   let complex;
 
   beforeEach(async () => {
     const Complex = await ethers.getContractFactory("Complex");
-    const complex = await Complex.deploy();
+    complex = await Complex.deploy();
   });
-  it("Should complex library: ", async () => {
+  it("Should deploy", async () => {
     await complex.deployed();
-
-    const tx = await complex.add(1, 1, 1, 1);
-    console.log(tx);
+    console.log(complex.address);
   });
 
-  it("Should add: ", async () => {
-    const tx = await complex.add(1, 1, 1, 1);
-    console.log(tx);
+  it("Should test math", async () => {
+    const test = new Complex(2, 2);
+    console.log(test);
+  });
+
+  it("Should add", async () => {
+    const re1 = ethers.utils.parseEther("1");
+    const im1 = ethers.utils.parseEther("1");
+    const re2 = ethers.utils.parseEther("1");
+    const im2 = ethers.utils.parseEther("1");
+
+    const result = await complex.add(re1, im1, re2, im2);
+    console.log(result);
+  });
+
+  it("Should add", async () => {
+    const re1 = ethers.utils.parseEther("1");
+    const im1 = ethers.utils.parseEther("1");
+    const re2 = ethers.utils.parseEther("1");
+    const im2 = ethers.utils.parseEther("1");
+
+    const result = await complex.sub(re1, im1, re2, im2);
+    console.log(result);
   });
 });
