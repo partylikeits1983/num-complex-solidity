d85e5fc203ea0c7002841ee06fc1af8543bcf55d
turing <aleqvids@gmail.com>
2023-01-24 13:32:39 +0300

    update types

---
+++ test/deploy.test.ts
@@ -0,0 +1,11 @@
+
+import { ethers } from "hardhat";
+
+describe("Deploy Num_Complex", () => {
+  it("Should complex library: ", async () => {
+    const Complex = await ethers.getContractFactory("Num_Complex");
+    const complex = await Complex.deploy();
+    await complex.deployed();
+    console.log("complex library:", complex.address);
+  });
+});
--- test/math.test.ts
+++ test/math.test.ts
@@ -6,7 +6,6 @@ import Complex from "complex.js";
 import { sqrt } from "mathjs";
 
 describe("Complex Number Library", () => {
-
   async function initialize() {
     const Complex = await ethers.getContractFactory("Num_Complex");
     const complex = await Complex.deploy();
@@ -17,15 +16,9 @@ describe("Complex Number Library", () => {
     const real = ethers.utils.parseEther(a.toString());
     const imag = ethers.utils.parseEther(b.toString());
 
-    return { complex, a, b, real, imag};
+    return { complex, a, b, real, imag };
   }
-
   describe("Complex Tests", function () {
-
-/*     it("Should deploy", async () => {
-      await complex.deployed();
-    });
- */
     it("Should add", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -47,11 +40,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re == re);
       expect(result_js.im == im);
     });
-
     it("Should subtract", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -73,11 +64,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re == re);
       expect(result_js.im == im);
     });
-
   it("Should multiply", async () => {
     const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -99,11 +88,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re == re);
       expect(result_js.im == im);
     });
-
     it("Should divide", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -125,11 +112,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result[1]),
         "ether"
       ));
-
       expect(result_js.re == re);
       expect(result_js.im == im);
     });
-
     it("Should calculate r2", async () => {
       const { complex, a, b } = await loadFixture(initialize);
 
@@ -146,10 +131,8 @@ describe("Complex Number Library", () => {
       const result = await complex.r2(re1, im1);
 
       const r2 = Number(ethers.utils.formatUnits(ethers.BigNumber.from(result), "ether"));
-
       expect(result_js == r2);
     });
-
     it("Should calculate non precise atan2", async () => {
       const { complex, a, b } = await loadFixture(initialize);
 
@@ -164,10 +147,8 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result),
         "ether"
       ));
-
       expect(result_js).to.be.closeTo(atan2, 0.1);
     });
-
     it("Should calculate precise atan2", async () => {
       const { complex, a, b } = await loadFixture(initialize);
 
@@ -182,10 +163,8 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result),
         "ether"
       ));
-
       expect(result_js).to.be.closeTo(atan2, 0.01);
     });
-
     it("Should calculate more precise atan2 (1 to 1)", async () => {
       const { complex } = await loadFixture(initialize);
 
@@ -201,10 +180,8 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result),
         "ether"
       ));
-
       expect(result_js).to.be.closeTo(atan2, 0.001);
     });
-
     it("Should calculate complex ln", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -216,7 +193,6 @@ describe("Complex Number Library", () => {
 
       const result_sol = await complex.ln(a1);
 
-
       const re = Number(ethers.utils.formatUnits(
         ethers.BigNumber.from(result_sol[0]),
         "ether"
@@ -225,11 +201,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re).to.be.closeTo(re, 0.005);
       expect(result_js.im).to.be.closeTo(im, 0.005);
     });
-
     it("Should calculate complex sqrt", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -249,11 +223,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re).to.be.closeTo(re, 0.003);
       expect(result_js.im).to.be.closeTo(im, 0.003);
     });
-
     it("Should calculate complex exponential", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -273,11 +245,9 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re).to.be.closeTo(re, 0.000001);
       expect(result_js.im).to.be.closeTo(im, 0.000001);
     });
-
     it("Should calculate complex power", async () => {
       const { complex, a, b, real, imag } = await loadFixture(initialize);
 
@@ -299,10 +269,8 @@ describe("Complex Number Library", () => {
         ethers.BigNumber.from(result_sol[1]),
         "ether"
       ));
-
       expect(result_js.re).to.be.closeTo(re, 0.02);
       expect(result_js.im).to.be.closeTo(im, 0.02);
     });
   });
-  
 });
